a9c8d5faa63faca7b17b5708f63864472125aa0c
Hents <hents_a@outlook.fr>
2020-11-28 20:24:35 +0100

    HEN : SHOP + ITEMS RELATED

--- source_code/src/code/item/Key.java
+++ source_code/src/code/item/Key.java
@@ -4,12 +4,12 @@ import code.enumeration.Level;
 
 public class Key extends Item {
 
-	private Level level;
+	final private Level level;
 
 	// CONSTRUCTOR
 
 	public Key(String name, int price, Level level) {
-		super("Copper key", price);
+		super(name, price);
 		this.level = level;
 		this.setDescription("TYPE: Key - " + level.toString() + " " + this.getName() + "\t| USE: This key allows you to unlock a " + level.toString() + " game.");
 	}
@@ -20,10 +20,10 @@ public class Key extends Item {
 		return level;
 	}
 
-	// METHODS
+	// OTHER METHODS
 
 	@Override
 	public void printItem() {
-		System.out.print("OBJECT: " + level.toString() + " "  + this.getName() + "\t| PRICE: " + this.getPrice() + " coins \t| USE: This key allows you to unlock a " + level.toString() + " game.");
+		System.out.println("TYPE: KEY\t| OBJECT: " + this.getLevel().toString() + " "  + this.getName() + "\t| PRICE: " + this.getPrice() + " coins \t| USE: This key allows you to unlock a " + this.getLevel().toString() + " game.");
 	}
 }
