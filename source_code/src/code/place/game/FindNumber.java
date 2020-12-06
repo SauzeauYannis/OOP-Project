d961c0f44b0a547368eb7594a35a5b7d6f661924
Yannis <sauzeauyannis@gmail.com>
2020-12-06 19:41:45 +0100

    Start comment find number

--- source_code/src/code/place/game/FindNumber.java
+++ source_code/src/code/place/game/FindNumber.java
@@ -10,61 +10,65 @@ import java.util.Scanner;
 
 public class FindNumber extends Game {
 
-    private final static int MAX_INT = 999;
+    /*****************************
+     * Attributes and constructor
+     *****************************/
+
+    // Class attributes
+    private final static int MAX_NUMBER = 999;
     private final static int DEFAULT_ATTEMPT = 10;
 
-    public FindNumber(String name, String description, NPC npc, Level level) {
-        super(name, description, npc, level);
-    }
-
+    // Constructor
     public FindNumber() {
-        this("Find Number",
+        super("Find Number",
                 "| In this game you need to find a number think by the man who is in front of you.\n" +
                 "| Type \"play\" to start the game.",
                 new NPC("Vincent Faygaf"),
                 Level.COPPER);
     }
 
+    /*********
+     * Method
+     *********/
+
+    // To play the game
     @Override
     public void play(Player player) {
+        // Method variables
         Scanner scanner = Gameplay.scanner;
-
         int attempt = DEFAULT_ATTEMPT;
-
-        int choose;
-        int rand = (int)(Math.random()*(MAX_INT));
+        int rand = (int) (Math.random() * (MAX_NUMBER));
+        int chosenNumber;
 
         System.out.println("\n--- Game launched ---\n");
 
-        this.getNpc().talk("You need to find my number between 0 and " +
-                MAX_INT +
-                " in " +
-                DEFAULT_ATTEMPT +
-                " attempts!");
+        this.getNpc().talk("You need to find my number between 0 and " + MAX_NUMBER + " in " + DEFAULT_ATTEMPT + " attempts!");
 
+        // While the player still has attempt
         while (attempt > 0) {
+
             System.out.print(player);
 
             try {
-                choose = scanner.nextInt();
+                chosenNumber = scanner.nextInt();
             } catch (Exception exception) {
                 scanner.nextLine();
-                choose = MAX_INT + 1;
                 this.getNpc().talk("You need to write a number!");
+                continue;
             }
 
-            if(choose > MAX_INT || choose < 0) {
+            if(chosenNumber > MAX_NUMBER || chosenNumber < 0) {
                 this.getNpc().talk("Please entry a valid number!");
             } else {
                 attempt--;
-                if (attempt == 0 && choose != rand) {
+                if (attempt == 0 && chosenNumber != rand) {
                     this.getNpc().talk("The number was " + rand);
                     this.lose(player);
                     break;
                 } else {
-                    if (rand > choose) {
+                    if (rand > chosenNumber) {
                         this.getNpc().talk("It's more!");
-                    } else if (rand < choose) {
+                    } else if (rand < chosenNumber) {
                         this.getNpc().talk("It's less!");
                     } else {
                         this.win(player);
