d473c833471801ea72fe1238a1796c26ecc35a0f
Hents <hents_a@outlook.fr>
2020-12-07 10:35:01 +0100

    HEN : Comment Karaoke + Hangman + Questions

--- source_code/src/code/place/game/Questions.java
+++ source_code/src/code/place/game/Questions.java
@@ -10,10 +10,15 @@ import java.util.Scanner;
 
 public class Questions extends Game {
 
+    /*****************************
+     * Attributes and constructor
+     *****************************/
+
+    //ATTRIBUTES
     private final static int NB_ROUND = 5; //Must be <= nb of questions
     private final static int DEFAULT_REWARD = 5;
 
-    // Here, correct answers are always put in index 1
+        // Here, correct answers are always put in index 1
     private final String[][] QUESTIONS_EASY = {
             {"Which famous dictator ruled the USSR from the mid-1920s to 1953 ?","Stalin","Lenin","Trotsky","Gorbachev"},
             {"In which country can we find Catalonia, Andalusia and Castile ?","Spain","Italy","France","Portugal"},
@@ -35,19 +40,20 @@ public class Questions extends Game {
             {"What is the capital of Zimbabwe ?", "Harare","Bulawayo","Mutare","Ouagadougou"}
     };
 
-
-    public Questions(String name, String description, NPC npc, Level level) {
-        super(name, description, npc, level);
-    }
-
+    //CONSTRUCTOR
     public Questions() {
-        this("Questions",
+        super("Questions",
                 "| In this game, your culture will be useful to find out the correct answer to questions.\n"  +
                         "| Type \"play\" to start the game.",
                 new NPC("Samuel Outienne"),
                 Level.PLATINUM);
     }
 
+    /**********
+     * Methods
+     **********/
+
+    //GETTERS
     public int getLengthQUESTEASY(){
         return this.QUESTIONS_EASY.length;
     }
@@ -56,6 +62,7 @@ public class Questions extends Game {
         return this.QUESTIONS_DIFFICULT.length;
     }
 
+    //OVERRIDE METHODS
     @Override
     public void play(Player player) {
         NPC npc = this.getNpc();
@@ -74,13 +81,11 @@ public class Questions extends Game {
 
         npc.talk("Right. Get ready, get set, go!");
 
-        // To initialize game
+        // To declare variable needed for the game
         int round = 1;
         boolean lose = false;
         boolean stop = false;
-
         int jackpot = 0;
-
         int[] quest_used = new int[NB_ROUND];
 
         // Here starts the game loop
@@ -95,10 +100,10 @@ public class Questions extends Game {
                 id_quest = randNum(QUESTIONS.length);
             } while (!(isUsableQuestionId(id_quest,round-1,quest_used)));
 
-            // Stocks id of used questions
+            // Stocks id of used question
             quest_used[round-1] = id_quest;
 
-            // Asks if the player wants to continue
+            // Asks if the player wants to continue if it is not the first loop
             if (round > 1) {
                 stop = wantStop(jackpot, npc, player);
             }
@@ -113,6 +118,7 @@ public class Questions extends Game {
                 // Displays question and answers
                 good_answer = displayQuestionBoard(id_quest,QUESTIONS);
 
+                // To get the player's answer
                 while ((answer!=1) && (answer!=2) && (answer!=3) && (answer!=4)) {
                     System.out.println("\t(TAPE: \"1\", \"2\", \"3\" or \"4\")");
 
@@ -127,6 +133,7 @@ public class Questions extends Game {
                     }
                 }
 
+                // Verifies if player's answer is good
                 if (answer != good_answer){
                     npc.talk("It was not the correct answer. Sorry, you lose the game and your jackpot.\n" +
                             "Next time will be the good one!");
@@ -141,25 +148,30 @@ public class Questions extends Game {
             }
         }
 
+        // Displays NPC reaction
         endGame(lose,round,npc);
 
+        // Checks if player wins and acts accordingly
         if (lose) {
             this.lose(player);
         } else {
             this.win(player, jackpot);
         }
 
+        // To flush scanner
         Gameplay.scanner.nextLine();
 
         System.out.println("\n--- Game finished ---\n");
     }
 
+    //To pick a random num
     public int randNum(int length){
         Random rd = new Random();
         rd.nextInt(length);
         return rd.nextInt(length);
     }
 
+    //Asks if player wants stop
     private boolean wantStop(int jackpot, NPC npc, Player player) {
         Scanner scanner = Gameplay.scanner;
         scanner.nextLine();
@@ -183,8 +195,10 @@ public class Questions extends Game {
        return stop;
     }
 
+    //Checks if question has been already used
     private boolean isUsableQuestionId(int id_quest, int index, int[] quest_used) {
         boolean found = true;
+        //Cheks in used id array if id chosen randomly exists
         if (index != 0) {
             for (int i = 0; i < index; i++) {
                 if (quest_used[i] == id_quest) {
@@ -196,12 +210,13 @@ public class Questions extends Game {
         return found;
     }
 
+    //Provides an questions array from level that player wants
     private String[][] getQUESTIONS(NPC npc, Player player){
         Scanner scanner;
-
         String lvl_chosen = "";
         String[][] QUESTIONS;
 
+        // Asks to the player level he/she wants
         while ((lvl_chosen.compareTo("EASY")!=0) && (lvl_chosen.compareTo("DIFFICULT")!=0)) {
             System.out.println("\t(TAPE: \"easy\" or \"difficult\")");
             scanner = Gameplay.scanner;
@@ -210,6 +225,7 @@ public class Questions extends Game {
             lvl_chosen = lvl_chosen.toUpperCase();
         }
 
+        // Create an array of easy or diffucult questions according to the choice
         if (lvl_chosen.compareTo("EASY")==0) {
             npc.talk("You choose \"easy\". Well, easily but surely!");
             QUESTIONS = new String[getLengthQUESTEASY()][];
@@ -223,13 +239,15 @@ public class Questions extends Game {
         return QUESTIONS;
     }
 
+    //Displays question and answers
     private int displayQuestionBoard(int id_quest, String[][] QUESTIONS) {
         String question = QUESTIONS[id_quest][0];
         System.out.println("\"" + question + "\"");
 
-        int rd = randNum(100);
+        int rd = randNum(100);      // To display randomly answers
         int num_answer = 1;
         int num_corr = -1;
+
         for (int i = rd; i < rd + 6; i++) {
             int index = i % 5;
             if (index != 0) {
@@ -240,7 +258,7 @@ public class Questions extends Game {
                                 " - " +
                                 QUESTIONS[id_quest][index]);
                         if (index == 1) {
-                            num_corr = num_answer;
+                            num_corr = num_answer;      // Stocks the number of the correct answer
                         }
                         break;
                     case 2:
@@ -250,7 +268,7 @@ public class Questions extends Game {
                                 " - " +
                                 QUESTIONS[id_quest][index]);
                         if (index == 1) {
-                            num_corr = num_answer;
+                            num_corr = num_answer;      // Stocks the number of the correct answer
                         }
                         break;
                     default:
@@ -261,6 +279,7 @@ public class Questions extends Game {
         return num_corr;
     }
 
+    //Gives NPC's reactions according to player's conditions
     private void endGame(boolean lose, int round, NPC npc) {
         if (round==NB_ROUND+1) {
             npc.talk("A faultless! You win the ultimate reward. A real winner, congratulations!");
@@ -270,13 +289,14 @@ public class Questions extends Game {
         npc.talk("I hope I will see soon ! :)");
     }
 
+    //Copies QUESTIONS_EASY to an targeted array
     private void cloneEASY(String[][] target, int length) {
         for (int i = 0; i < length; i++) {
             target[i] = QUESTIONS_EASY[i].clone();
         }
     }
 
-
+    //Copies QUESTIONS_DIFFICULT to an targeted array
     private void cloneDIFFICULT(String[][] target, int length) {
         for (int i = 0; i < length; i++) {
             target[i] = QUESTIONS_DIFFICULT[i].clone();
