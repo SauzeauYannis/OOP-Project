b2b17ef4abf256475e70f766837b48b81de8e9f1
Samuel <61194052+Strikza@users.noreply.github.com>
2020-12-07 10:07:22 +0100

    Comment Riddle

--- source_code/src/code/place/game/Riddle.java
+++ source_code/src/code/place/game/Riddle.java
@@ -11,10 +11,12 @@ import java.util.Scanner;
 
 public class Riddle extends Game {
 
+    /******************************
+     * Attributes and constructor *
+     ******************************/
+
     /// Constants ///
-
     final static int DEFAULT_ATTEMPTS = 3;
-
     final static String[][] RIDDLES ={
             {"What is the summation between 1 and 2 ?", "3"},
             {"What answer Zebi if his professor ask who want to the b ?","can I do the b"},
@@ -23,36 +25,36 @@ public class Riddle extends Game {
             {"You have this suite : 2 3 5 ? 11 ", "7"}
     };
 
-
     /// Attributes ///
-
     private int attempts;
 
-
-    /// Methods ///
-
-    public Riddle(String name, String description, NPC npc, Level level) {
-        super(name, description, npc, level);
-        this.attempts = DEFAULT_ATTEMPTS;
-    }
-
+    // Constructor
     public Riddle() {
-        this("Riddle",
+        super("Riddle",
                 "| Here you need to answer to a riddle. You have only ??? attempts!\n" +
                         "| Turn on your brain, and make it work hard!" +
                         "| Type \"play\" to start the game.",
                 new NPC("Jean-Pierre Fougas"),
                 Level.GOLD);
+        this.attempts = DEFAULT_ATTEMPTS;
     }
+
+
+    /***********
+     * Methods *
+     ***********/
 
     @Override
     public void play(Player player) {
 
         Scanner scan = Gameplay.scanner;
-        Random rand = new Random();
-        String[] riddle = RIDDLES[rand.nextInt(RIDDLES.length)];
         String answer;
 
+        // Choose a random riddle
+        Random rand = new Random();
+        String[] riddle = RIDDLES[rand.nextInt(RIDDLES.length)];
+
+        // Initialize the number of attempts (useless for the first time, but useful for the remain
         this.attempts = DEFAULT_ATTEMPTS;
 
         System.out.println("\n--- Game launched ---\n");
@@ -62,15 +64,19 @@ public class Riddle extends Game {
                 "But you know, I am a professional in this domain! I have 50 years... bla bla bla...\n" +
                 "... but I am remember this guy, Ethoufet Kwallah, my first rival in... bla bla bla...\n" +
                 "... and this is how I am became what I am today! Wait you're sleeping during my story?!\n" +
-                "Ooooh, like each time. Ok so, are you read?\n");
+                "Ooooh, like each time. Ok so, are you ready ?\n");
 
+        // while attempts isn't equal to 0
         while(attempts != 0){
 
-            System.out.println("-> Choice (yes or no): ");
             System.out.print(player);
+            System.out.println("-> Choice (yes or no): ");
             answer = scan.nextLine().toLowerCase();
 
+            // Check if the answer is valid or not
             if(answer.equals("yes") || answer.equals("no")){
+
+                // Check if the player is a joker
                 if(answer.equals("no")){
                     this.getNpc().talk("Oh...Ok...So, goodbye kid. I hope you will change\n" +
                             " your opinion, and come back traveler...");
@@ -83,19 +89,26 @@ public class Riddle extends Game {
 
                     this.getNpc().talk("So traveler, what is your answer? O.O");
 
+                    // While attempts isn't equal to 0
                     while(attempts != 0){
-                        System.out.println("-> Answer: ");
                         System.out.print(player);
+                        System.out.println("-> Answer: ");
                         answer = scan.nextLine().toLowerCase();
 
+                        // Check the answer for the riddle
                         if(answer.equals(riddle[1].toLowerCase())){
                             this.getNpc().talk("INCREDIBLE TRAVELER !!!!\n" +
                                     "You are a true Hercule Kourge ! (it isn't Poirot ?)\n" +
                                     "You have deserved your reward traveler! Take it!");
                             this.win(player);
+
+                            // To go out of the loop
                             attempts = 0;
                         }
+
+                        // Check if this is the last attempt
                         else if(attempts > 1){
+                            // Remove 1 attempt
                             attempts -= 1;
 
                             this.getNpc().talk("Ok traveler, this isn't the end ! You can take sometime\n" +
@@ -103,6 +116,7 @@ public class Riddle extends Game {
                             System.out.println("| Attempts left : " + attempts + "\n");
                         }
                         else {
+                            // Remove the last attempt, to arrive to 0
                             attempts -= 1;
 
                             this.getNpc().talk("Oh no traveler, this isn't the good answer ...\n" +
@@ -117,7 +131,7 @@ public class Riddle extends Game {
             }
             else{
                 this.getNpc().talk("What did you said ? Sorry but I am an old man you know ... bla bla bla ...\n" +
-                        "... and this is how I am became what I am today ! OH ! So-Sorry, I have started again.\n" +
+                        "... and this is how I am became what I am today ! OH ! So-Sorry, I have started again...\n" +
                         "So ?\n");
             }
         }
